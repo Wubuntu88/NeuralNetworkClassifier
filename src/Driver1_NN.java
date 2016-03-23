6848a13a868c99e989a04d0a486c2cba6ab2c692
William Gillespie <wgilles1@emich.edu>
2016-03-23 13:08:33 -0400

    Testing new stuff

--- src/Driver1_NN.java
+++ src/Driver1_NN.java
@@ -5,11 +5,14 @@ public class Driver1_NN {
 		
 		String fileName = "part2/myTrain1";
 		NeuralNetworkIO nnIO = new NeuralNetworkIO();
+		NeuralNetworkClassifier nnc = null;
 		try {
-			nnIO.instantiateNNClassifierWithTrainingData(fileName);
+			nnc = nnIO.instantiateNNClassifierWithTrainingData(fileName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		nnc.printRecords();
 	}
 }
 
