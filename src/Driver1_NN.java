6df163927c2917872a4a94dc1d2d3e43bbca0e98
WilliamGillespie <wgilles1@emich.edu>
2016-03-24 17:51:07 -0400

    finished

--- src/Driver1_NN.java
+++ src/Driver1_NN.java
@@ -6,6 +6,8 @@ public class Driver1_NN {
 		
 		String trainingFileName = "part2/myTrain1";
 		String testFileName = "part2/test1";
+		String validationFileName = "part2/validate1";
+		String validationOutputFileName = "part2/validationOutput1";
 		String outputFile = "part2/output1";
 		NeuralNetworkIO nnIO = new NeuralNetworkIO();
 		NeuralNetworkClassifier nnc = null;
@@ -15,17 +17,21 @@ public class Driver1_NN {
 			e.printStackTrace();
 		}
 		
-		nnc.printRecords();
+		//nnc.printRecords();
 		int numberOfHiddenNodes = 4;
 		int seed = 123456;
 		int iterations = 10000;
 		double learningRate = .7;
+		System.out.println("part2 of neural network");
+		System.out.println("parametets:");
+		System.out.println("# hidden nodes:" + numberOfHiddenNodes);
+		System.out.println("seed: " + seed);
+		System.out.println("iterations: " + iterations);
+		System.out.println("learning rate: " + .7);
 		nnc.setParameters(numberOfHiddenNodes, seed, iterations, learningRate);
-		
 		nnc.train();
-		double trainError = nnc.calculateTrainingError();
-		System.out.println("training error: " + trainError);
 		
+		/* A Classification of records in the test file */
 		ArrayList<Record> testRecords = null;
 		try {
 			testRecords = nnIO.readTestRecordsFromFile(testFileName);
@@ -34,23 +40,31 @@ public class Driver1_NN {
 			e.printStackTrace();
 		}
 		ArrayList<Double> classifications = nnc.classifyRecords(testRecords);
-		for(Record record: testRecords){
-			System.out.println(record);
-		}
-		for(Double d: classifications){
-			System.out.println(d);
-		}
-		
 		nnIO.writeRecordsToFile(outputFile, testRecords, classifications);
+		/* B computing training error of file */
+		double trainError = nnc.calculateTrainingError();
+		System.out.println("training error: " + trainError);
 		
-		//nnc.initializeNetwork();
+		/* C validation error */
+		ArrayList<Record> validationRecords = null;
+		try {
+			validationRecords = nnIO.readTestRecordsFromFile(validationFileName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ArrayList<Double> valClassifications = nnc.classifyRecords(validationRecords);
+		double validationError = nnIO.findValidationErrorRate(validationRecords, valClassifications);
+		System.out.println("validation error: " + validationError);
 		
-		/*
+		nnIO.writeRecordsToFile(validationOutputFileName, validationRecords, valClassifications);
+
+		/* D printing the weights and the theta values */
 		nnc.printInputToHiddenWeigths();
 		nnc.printOutputWeights();
 		nnc.printInputToMiddleThetas();
 		nnc.printOutputThetas();
-		*/
 	}
 }
 
