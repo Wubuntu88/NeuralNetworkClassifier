aaf1ce04712007006cf224ae607e190d96de0615
William Gillespie <wgilles1@emich.edu>
2016-03-23 20:20:42 -0400

    testing

--- src/Driver1_NN.java
+++ src/Driver1_NN.java
@@ -1,33 +1,56 @@
+import java.util.ArrayList;
 
 public class Driver1_NN {
 	
 	public static void main(String[] args) {
 		
-		String fileName = "part2/myTrain1";
+		String trainingFileName = "part2/myTrain1";
+		String testFileName = "part2/test1";
+		String outputFile = "part2/output1";
 		NeuralNetworkIO nnIO = new NeuralNetworkIO();
 		NeuralNetworkClassifier nnc = null;
 		try {
-			nnc = nnIO.instantiateNNClassifierWithTrainingData(fileName);
+			nnc = nnIO.instantiateNNClassifierWithTrainingData(trainingFileName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		nnc.printRecords();
-		int numberOfHiddenNodes = 3;
+		int numberOfHiddenNodes = 4;
 		int seed = 123456;
-		int iterations = 1000;
-		double learningRate = 0.01;
+		int iterations = 10000;
+		double learningRate = .7;
 		nnc.setParameters(numberOfHiddenNodes, seed, iterations, learningRate);
 		
 		nnc.train();
+		double trainError = nnc.calculateTrainingError();
+		System.out.println("training error: " + trainError);
+		
+		ArrayList<Record> testRecords = null;
+		try {
+			testRecords = nnIO.readTestRecordsFromFile(testFileName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<Double> classifications = nnc.classifyRecords(testRecords);
+		for(Record record: testRecords){
+			System.out.println(record);
+		}
+		for(Double d: classifications){
+			System.out.println(d);
+		}
+		
+		nnIO.writeRecordsToFile(outputFile, testRecords, classifications);
+		
 		//nnc.initializeNetwork();
 		
-		nnc.letsPrintTheClassifiedRecords();
-		
+		/*
 		nnc.printInputToHiddenWeigths();
 		nnc.printOutputWeights();
 		nnc.printInputToMiddleThetas();
 		nnc.printOutputThetas();
+		*/
 	}
 }
 
